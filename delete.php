a10fa58bb31ed12723631002be4872e20e16a4af
dude613 <liamswift2014@gmail.com>
2020-05-06 09:49:28 -0400

    fix_delete_if-else

--- delete.php
+++ delete.php
@@ -1,3 +1,10 @@
+<head>
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="app.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+
 <?php
 
 require 'db.php';
@@ -8,12 +15,23 @@ $sql = "delete from tasks where ID = '$id'";
 
 $val = $db->query($sql);
 
-if($val){
+if ($val) {
+    echo '<div> <br> </div>';
+
+    echo 'deleted';
 
     echo '<div> <br> </div>';
 
-echo 'deleted';
+    echo "<a href='index.php' class='btn btn-success'>Home</a>";
 
+} else {
+    echo '<div> <br> </div>';
+    
+        echo 'deletion failed'; 
+    
+        echo '<div> <br> </div>';
+    
+        echo "<a href='index.php' class='btn btn-success'>Home</a>";
 };
 
 ?>
