ee8ac86a5e737bcf4d791b5b885a482addd7c3f1
dude613 <liamswift2014@gmail.com>
2020-05-06 09:06:06 -0400

    add.php

--- add.php
+++ add.php
@@ -1,3 +1,10 @@
+<head>
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="app.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+
 <?php 
 
 include 'db.php';
@@ -12,6 +19,8 @@ if(isset($_POST['send'])){
 
     if($val){
         echo "<h1>Successful</h1>";
+
+        echo "<a href='index.php' class='btn btn-success'>Home</a>";
     }
     else {
         echo "<h1>Failed</h1>";
