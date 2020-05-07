2d22487071c2df9b8ee67ae075fb54dfcff14262
dude613 <liamswift2014@gmail.com>
2020-05-06 20:08:52 -0400

    pagination_start

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <!doctype html>
 <html lang="en">
 
-<?php require 'db.php';
+<?php include 'db.php';
 
-$page = (isset($GET['page']) ? $GET['page'] : 1);
+$page = (isset($_GET['page']) ? $_GET['page'] : 1);
+$perPage = (isset($_GET['per-page']) && ($_GET['per-page']) <= 50 ? $_GET['per-page'] : 5);
+$start = ($page > 1) ? ($page * $perPage) - $perPage :0 ;
 
-$perPage = (isset($GET['per-page']) && ($GET['per-page']) <= 50 ? $GET['per-page'] : 5);
-
-$start = ($page > 1) ? ($page * $perPage) - $perPage : 0;
-
-$sql = "select * from tasks limit ".$start.",".$perPage." ";
-
-$totalRecords = $db->query("select * from tasks")->num_rows;
-
-$pages = ceil($totalRecords / $perPage);
+$sql = "select * from tasks limit ".$start." , ".$perPage." ";
+$total = $db->query("select * from tasks")->num_rows;
+$pages = ceil($total / $perPage);
 
 $rows = $db->query($sql);
 
 ?>
+
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -40,7 +37,7 @@ $rows = $db->query($sql);
         <button type="button" data-target="#myModal" data-toggle="modal" class="btn btn-success">Add Task</button>
       </div>      
       <div class="ml-auto">
-        <button type="button" class="btn btn-default">Print</button>
+        <button type="button" class="btn btn-primary">Print</button>
         <br>
       </div>
     </div>
@@ -95,9 +92,14 @@ $rows = $db->query($sql);
         </tbody>
     </table>
     <div>
-                <ul class="pagination justify-content-center">
-                <li class="page-item"><a class="page-link" href="">1</a></li>
-                </ul>
+        <ul class="pagination justify-content-center">
+
+        <?php for($i = 1 ; $i <= $pages; $i++): ?>
+        <li class="page-item"><a class="page-link" href="?pages=<?php echo $i; ?>&per-page<?php echo $perPage; ?>"><?php echo $i; ?></a></li>
+
+        <?php endfor; ?>
+
+        </ul>
     </div>
   </div>
     <!-- Optional JavaScript -->
